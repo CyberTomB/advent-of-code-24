c15d7b8a81c343587cbc4b61321155d509d3bfe0
CyberTomB <tomcbowers@gmail.com>
2024-12-09 22:06:07 -0700

    initialize main.py for day 3

---
+++ day_3/main.py
@@ -0,0 +1,18 @@
+EXAMPLE_DATA = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+MORE_EXAMPLE_DATA = "mul(4*|mul(6,9!?(12,34)mul ( 2, 4 )"
+EXAMPLE_SOLUTION = 161
+
+def find_real_muls(string: str) -> list[tuple[int, int]]:
+    pass
+
+
+def mul(x, y):
+    return x * y
+
+def main():
+    print("Running Day 3...")
+    mul_tuples = []
+
+
+if __name__ == "__main__":
+    main()
