34745e4bda86306818dd9c141d850637ba3b2533
CyberTomB <tomcbowers@gmail.com>
2024-12-11 09:50:45 -0700

    wow numpy is a great library -- solved part 2 on first try!

--- day_4/main.py
+++ day_4/main.py
@@ -12,12 +12,6 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX"""
 
-# 3 vertial
-# 5 horizontal
-# 10 diagonal
-
-SAMPLE_AS_LIST = SAMPLE_DATA.split("\n")
-
 def vertical_transpose(rows):
     verticals = {}
     for i in range(0, len(rows)):
@@ -30,18 +24,8 @@ def vertical_transpose(rows):
     return list(verticals.values())
 
 def diagonal_transpose(rows):
-    matrix = []
+    matrix, maximum = convert_to_matrix(rows)
     diagonals = []
-    row_length = 0
-    for row in rows:
-        row_arr = []
-        for char in row:
-            if(not char == '\n'):
-                row_arr.append(char)
-        matrix.append(row_arr)
-        row_length = len(row_arr)
-    maximum = max(row_length, len(matrix))
-    print(f"The max is {maximum}")
 
 
     for i in range(0, (maximum - 3)):
@@ -63,27 +47,42 @@ def diagonal_transpose(rows):
 
     return diagonals
         
-
-
-
 def find_xmas(string) -> int:
    print(f">>>Trying to find XMAS in {string}")
    xmases = findall(r'(XMAS)', string)
    samxes = findall(r'(SAMX)', string)
    print(f">> FOUND {xmases} and {samxes}")
    return len(xmases) + len(samxes)
-        
 
+def convert_to_matrix(rows):
+    matrix = []
+    row_length = 0
+    for row in rows:
+        row_arr = []
+        for char in row:
+            if(not char == '\n'):
+                row_arr.append(char)
+        matrix.append(row_arr)
+        row_length = len(row_arr)
+    maximum = max(row_length, len(matrix))
+    return matrix, maximum
 
-def main():
-    with open('data.txt') as f:
-        data = f.readlines()
+def rolling_window(rows):
+    matrix, maximum = convert_to_matrix(rows)
 
-    with open('sample.txt') as s:
-        sample = s.readlines()
+def find_crossmas(matrix):
+    # Expect a matrix of 3x3 such as:
+    # [M, A, S]
+    # [X, A, S]
+    # [M, S, S]
+    # Output a 1 because MAS x MAS in the center
+    top_left_bottom_right = ''.join(np.diagonal(matrix, 0))
+    top_right_bottom_left = ''.join(np.fliplr(matrix).diagonal(offset=0))
+    result = ((top_left_bottom_right == 'SAM' or top_left_bottom_right == 'MAS') and 
+    (top_right_bottom_left == 'SAM' or top_right_bottom_left == 'MAS'))
+    return int(result)
 
-    input = data
-
+def part_one(input):
     lines = input
     verticals = vertical_transpose(input)
     diagonals = diagonal_transpose(input)
@@ -107,6 +106,30 @@ def main():
     print(f">>> FOUND XMAS {diags_total} times in diagonal rows")
 
     print(f">>> GRAND TOTAL: {lines_total + diags_total + vertical_total}")
+
+
+def main():
+    with open('data.txt') as f:
+        data = f.readlines()
+
+    with open('sample.txt') as s:
+        sample = s.readlines()
+
+    input = data
+
+    matrix, maximum = convert_to_matrix(input)
+
+    window = np.lib.stride_tricks.sliding_window_view(matrix, (3, 3))
+    count = 0
+    for iteration in window:
+        for submatrix in iteration:
+            count += find_crossmas(submatrix)
+
+    print(f"Found {count} total crossmasses")
+            
+    
+
+
 
 if __name__ == "__main__":
     main()
