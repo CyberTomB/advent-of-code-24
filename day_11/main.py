c399a8a0cd291627fdfbdf0c610232b8fabb23d8
CyberTomB <tomcbowers@gmail.com>
2024-12-11 10:08:34 -0700

    jump ahead to day 11

--- day_11/main.py
+++ day_11/main.py
@@ -1,5 +1,26 @@
+sample = [0, 1, 10, 99, 999]
+data = [4, 4841539, 66, 5279, 49207, 134, 609568, 0]
+
+max_blinks = 25
+
+def replace_0_with_1(num):
+    if(num == 0):
+        return 1
+    else:
+        return num
+    
+def split_evens(num):
+    conversion = str(num)
+    length_of_digits = len(conversion)
+    if(not length_of_digits % 2 == 0):
+        return num
+    else:
+        half = length_of_digits / 2
+        return int(conversion[:half]), int(conversion[half:])
+
+
 def main():
-    pass
+    print(split_evens(2024))
 
 if __name__ == "__main__":
     main()
